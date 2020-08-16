eb5ebcbaf580e784e07a4206521cefe62a685809
kyle-law <junwei_1121@hotmail.com>
2020-08-17 03:31:30 +0800

    Refined Styling and layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to articles_url, notice: "You're now signed out!"
+    redirect_to root_path, notice: "You're now signed out!"
   end
 end
