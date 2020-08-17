87730c6202df62fe93427964239e1964379765f8
kyle-law <junwei_1121@hotmail.com>
2020-08-17 23:34:57 +0800

    Setup AWS S3 Bucket

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'carrierwave'
 gem 'cloudinary'
 gem 'figaro'
+gem 'fog-aws'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,14 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_ACCESS_KEY']
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory = ENV['S3_BUCKET']
-  end
+CarrierWave.configure do |config|
+
+  config.fog_credentials = {
+    provider:              'AWS',                            # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
+    region:                'us-east-1'                       # to match the carrierwave and bucket region
+  }
+  config.fog_directory = ENV["AWS_BUCKET"]                   # required
+  config.fog_public    = false
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on Heroku
+  config.storage       = :fog
+
 end
