e029554cc444befe47468364e120d25219c6b1b6
kyle-law <junwei_1121@hotmail.com>
2020-08-17 23:22:10 +0800

    Refinement

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider => 'AWS,
+      :provider => 'AWS',
       :aws_access_key_id => ENV['S3_ACCESS_KEY']
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
