a629b63e392ec838ed58aa87be2d91d94dfdff35
kyle-law <junwei_1121@hotmail.com>
2020-08-17 23:19:17 +0800

    Refinement

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.includes(:creator).find(params[:id])
     @vote = current_user.votes.find_by(article_id: @article.id) if current_user
